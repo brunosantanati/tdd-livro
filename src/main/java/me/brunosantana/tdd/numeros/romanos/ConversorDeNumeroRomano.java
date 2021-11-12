321ae194621ed874c3f1294d418f03f966d1254a
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2021-11-12 00:20:27 -0300

    Fazer testes passarem com logica de subtracao.

--- src/main/java/me/brunosantana/tdd/numeros/romanos/ConversorDeNumeroRomano.java
+++ src/main/java/me/brunosantana/tdd/numeros/romanos/ConversorDeNumeroRomano.java
@@ -17,9 +17,28 @@ public class ConversorDeNumeroRomano {
             }};
 
     public int converte(String numeroEmRomano) {
+        /*
+        Repare que os símbolos em um numeral romano, da direita para a esquerda, sempre crescem.
+        Quando um número a esquerda é menor do que seu vizinho a direita,
+        esse número deve então ser subtraído ao invés de somado no acumulador.
+         */
         int acumulador = 0;
-        for(int i = 0; i < numeroEmRomano.length(); i++) {
-            acumulador += tabela.get(numeroEmRomano.charAt(i));
+        int ultimoVizinhoDaDireita = 0;
+
+        for (int i = numeroEmRomano.length() - 1; i >= 0; i--) {
+
+            // pega o inteiro referente ao simbolo atual
+            int atual = tabela.get(numeroEmRomano.charAt(i));
+
+            // se o da direita for menor, o multiplicaremos
+            // por -1 para torná-lo negativo
+            int multiplicador = 1;
+            if (atual < ultimoVizinhoDaDireita) multiplicador = -1;
+
+            acumulador += tabela.get(numeroEmRomano.charAt(i)) * multiplicador;
+
+            // atualiza o vizinho da direita
+            ultimoVizinhoDaDireita = atual;
         }
         return acumulador;
     }
