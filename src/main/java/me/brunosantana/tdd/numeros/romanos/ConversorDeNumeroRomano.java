cddd03ba5a5225a954c4a145cc86dbc5d5defe4d
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2021-11-11 23:27:34 -0300

    Fazer os testes passarem para I e V.

--- src/main/java/me/brunosantana/tdd/numeros/romanos/ConversorDeNumeroRomano.java
+++ src/main/java/me/brunosantana/tdd/numeros/romanos/ConversorDeNumeroRomano.java
@@ -3,7 +3,10 @@ package me.brunosantana.tdd.numeros.romanos;
 public class ConversorDeNumeroRomano {
 
     public int converte(String numeroEmRomano) {
-        return 1;
+        if(numeroEmRomano.equals("I")) return 1;
+        else if(numeroEmRomano.equals("V")) return 5;
+        
+        return 0;
     }
 
 }
