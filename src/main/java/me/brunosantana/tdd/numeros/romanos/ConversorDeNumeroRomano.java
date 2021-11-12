0a185413608b272b86e59bc912fdb3202b6095e5
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2021-11-11 23:32:18 -0300

    Fazer passar demais casos (X, L, C, etc).

--- src/main/java/me/brunosantana/tdd/numeros/romanos/ConversorDeNumeroRomano.java
+++ src/main/java/me/brunosantana/tdd/numeros/romanos/ConversorDeNumeroRomano.java
@@ -1,12 +1,23 @@
 package me.brunosantana.tdd.numeros.romanos;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ConversorDeNumeroRomano {
 
+    private static Map<String, Integer> tabela =
+            new HashMap<String, Integer>() {{
+                put("I", 1);
+                put("V", 5);
+                put("X", 10);
+                put("L", 50);
+                put("C", 100);
+                put("D", 500);
+                put("M", 1000);
+            }};
+
     public int converte(String numeroEmRomano) {
-        if(numeroEmRomano.equals("I")) return 1;
-        else if(numeroEmRomano.equals("V")) return 5;
-        
-        return 0;
+        return tabela.get(numeroEmRomano);
     }
 
 }
